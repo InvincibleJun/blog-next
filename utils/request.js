8df14deca08e2b074a468868fae68522db7c85c5
chenjun <398955404@qq.com>
2018-05-08 09:40:34 +0800

    修改npm命令

--- utils/request.js
+++ utils/request.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'development' ? '/api' : ''
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://120.78.222.240:10086/api'
+    : '/api'
 
 export default function (url, arg = {}) {
   const { method = 'get', params, data } = arg
