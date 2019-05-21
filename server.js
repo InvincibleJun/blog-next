a100aca8a041ed5ce909918744d7d042e2fea885
chenjun <398955404@qq.com>
2019-05-21 18:36:41 +0800

    change-server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const next = require("next");
 const fs = require('fs');
-try {
-  const app = next({ dev: process.env.NODE_ENV === "development" });
-  const handle = app.getRequestHandler();
-  app
+
+const app = next({ dev: process.env.NODE_ENV === "development" });
+const handle = app.getRequestHandler();
+
+app
   .prepare()
   .then(() => {
     const server = express();
@@ -30,40 +31,6 @@ try {
     });
   })
   .catch(ex => {
-    console.log(ex);
-    // process.exit(1);
+    console.log(ex.stack);
+    process.exit(1);
   });
-} catch(e) {
-  console.log(e)
-}
-
-
-// app
-//   .prepare()
-//   .then(() => {
-//     const server = express();
-
-//     server.get("/favicon.ico", (req, res) => {
-//       res.sendFile("./staitc/favicon.ico");
-//     });
-
-//     server.get("/blog/:_id", (req, res) => {
-//       const queryParams = { _id: req.params._id };
-//       app.render(req, res, "/blog/article", queryParams);
-//     });
-
-//     server.get("*", (req, res) => handle(req, res));
-    
-
-
-//     server.listen(8000, err => {
-//       if (err) {
-//         throw err;
-//       }
-//       console.log("> next start at 8000");
-//     });
-//   })
-//   .catch(ex => {
-//     console.log(ex);
-//     // process.exit(1);
-//   });
